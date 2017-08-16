c1645fd6c648ce7e757593b6b6996f1e28d999dd
Till Kolditz <Till.Kolditz@gmail.com>
2017-08-16 17:18:07 +0200

    update outdated run.sh (no need for the awk transformation any longer) add runbench.sh (basically to keep that specific benchmark call around)

--- run.sh
+++ run.sh
@@ -6,10 +6,9 @@
 #########
 # build #
 #########
-rm -Rf build
-mkdir build
-pushd build
-(cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-6 .. && make -j $(nproc)) || exit 1
+./bootstrap.sh
+pushd build/Release
+(cmake -DCMAKE_BUILD_TYPE=Release .. && make -j $(nproc)) || exit 1
 
 
 
@@ -17,7 +16,8 @@ pushd build
 # normal #
 ##########
 ./benchmark >benchmark.out 2>benchmark.err
-awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmark.out >benchmark.out2
+# the following is not needed anymore since the relative runtimes are output by the benchmark
+# awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmark.out >benchmark.out2
 ln -s benchmark.out2 benchmark.csv
 HAS_AVX2=$(grep -m 1 flags /proc/cpuinfo | grep avx2)
 if [[ -z "${HAS_AVX2}" ]]; then
@@ -39,7 +39,8 @@ rm benchmark.csv
 # OMP (2 threads only) #
 ########################
 ./benchmarkOMP2 >benchmarkOMP2.out 2>benchmarkOMP2.err
-awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmarkOMP2.out >benchmarkOMP2.out2
+# the following is not needed anymore since the relative runtimes are output by the benchmark
+#awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmarkOMP2.out >benchmarkOMP2.out2
 ln -s benchmarkOMP2.out2 benchmark.csv
 if [[ -z "${HAS_AVX2}" ]]; then
         gnuplot ../plot_encode.m
@@ -60,7 +61,8 @@ rm benchmark.csv
 ###############################
 if [[ 2 -ne "$(nproc)" ]]; then
 	./benchmarkOMP >benchmarkOMP.out 2>benchmarkOMP.err
-	awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmarkOMP.out benchmarkOMP.out2
+	# the following is not needed anymore since the relative runtimes are output by the benchmark
+	#awk 'BEGIN{FS=",";} NF {if ($1 ~ /^(#|[^0-9])/){print;next}; if ($2 ~ /^[0-9]+\.[0-9]+$/) {print; next} {printf "%d",$1; for (i=2; i<=NF; i++){printf ",%.2f",($i/1000000000)}{printf "\n"}}; next} !NF {print}' benchmarkOMP.out benchmarkOMP.out2
 	ln -s benchmarkOMP.out2 benchmark.csv
 	if [[ -z "${HAS_AVX2}" ]]; then
         	gnuplot ../plot_encode.m
