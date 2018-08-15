2f5a1b76771b0384b8ac9c34284b824a5d198d98
Till Kolditz <till.kolditz@gmail.com>
2018-08-15 22:56:56 +0200

    make use only as many processors as available
    Signed-off-by: Till Kolditz <till.kolditz@gmail.com>

--- runbench.sh
+++ runbench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd build/Release
-make -j8 && (reset; ./benchmark 1> >(tee benchmark.out) 2> >(tee benchmark.err >&2); ./benchmark_novec 1> >(tee benchmark_novec.out) 2> >(tee benchmark_novec.err >&2))
+make -j$(nproc) && (reset; ./benchmark 1> >(tee benchmark.out) 2> >(tee benchmark.err >&2); ./benchmark_novec 1> >(tee benchmark_novec.out) 2> >(tee benchmark_novec.err >&2))
 popd
 
